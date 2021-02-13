917472e3f10cf20971848c262a7f0412cc70db10
bruno-wolf <bruno.m.lobato@gmail.com>
2021-02-13 12:53:16 +0000

    adds button to activity complication

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,11 @@
 //
+// initial fade-in
+setTimeout(() => {
+  document.getElementById('watch').classList.add('show');
+}, 1000);
+
+//
+// handling date
 function updateClock(hours, minutes, seconds, weekday, month) {
   const weekDays = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
   const hourHand = document.getElementById('hour');
@@ -99,27 +106,30 @@ setInterval(() => {
   updateClock(hours, minutes, seconds, weekday, month);
 }, 1000);
 
-setTimeout(() => {
-  document.getElementById('watch').classList.add('show');
-}, 1000);
-
 //
+// handling Activity Rings
 function randomActivityRings() {
   const moveRing = document.querySelector('.move-ring .completed');
   const exerciseRing = document.querySelector('.exercise-ring .completed');
   const standRing = document.querySelector('.stand-ring .completed');
 
-  const randomValue1 = getRandomInt(80);
-  const randomValue2 = getRandomInt(randomValue1);
-  const randomValue3 = getRandomInt(90);
+  const random1 = getRandomInt(80);
+  const random2 = getRandomInt(random1);
+  const random3 = getRandomInt(90);
 
-  moveRing.setAttribute('stroke-dasharray', `${randomValue1}, 100`);
-  exerciseRing.setAttribute('stroke-dasharray', `${randomValue2}, 100`);
-  standRing.setAttribute('stroke-dasharray', `${randomValue3}, 100`);
-
-  function getRandomInt(max) {
-    return Math.floor(Math.random() * Math.floor(max));
-  }
+  moveRing.setAttribute('stroke-dasharray', `${random1}, 100`);
+  exerciseRing.setAttribute('stroke-dasharray', `${random2}, 100`);
+  standRing.setAttribute('stroke-dasharray', `${random3}, 100`);
 }
 
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+const bottomComplicationButton = document.getElementById('bottom');
+
 randomActivityRings();
+
+bottomComplicationButton.addEventListener('click', () => {
+  randomActivityRings();
+});
