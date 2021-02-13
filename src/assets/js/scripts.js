d99257d4dbe907f348ba0d398ee86dce912733a8
bruno-wolf <bruno.m.lobato@gmail.com>
2021-02-13 01:07:09 +0000

    half way there

---
+++ src/assets/js/scripts.js
@@ -0,0 +1,38 @@
+//
+function updateClock(hours, minutes, seconds) {
+  const hourHand = document.getElementById('hour');
+  const minuteHand = document.getElementById('minute');
+  const secondHand = document.getElementById('second');
+  let m = minutes;
+  let h = hours;
+  let s = seconds;
+
+  h > 12 ? (h = h - 12) : h;
+  h = h * 30;
+  m = m * 6;
+  s = s * 6;
+
+  hourHand.setAttribute(
+    'style',
+    `transform: translate(-50%, -50%) rotate(${h}deg)`,
+  );
+
+  minuteHand.setAttribute(
+    'style',
+    `transform: translate(-50%, -50%) rotate(${m}deg)`,
+  );
+
+  secondHand.setAttribute(
+    'style',
+    `transform: translate(-50%, -50%) rotate(${s}deg)`,
+  );
+}
+
+setInterval(() => {
+  const time = new Date();
+  const hours = time.getHours();
+  const minutes = time.getMinutes();
+  const seconds = time.getSeconds();
+
+  updateClock(hours, minutes, seconds);
+}, 1000);
