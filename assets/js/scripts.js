5ab9fe964bbb757103a7195e17be61a0b72d0aa8
bruno-wolf <bruno.m.lobato@gmail.com>
2021-02-13 18:44:58 +0000

    adjusts viewport units on mobile

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,17 @@
 //
+// adjust viewport units on mobile
+const windowHeight = window.innerHeight;
+const current_scroll = window.scrollY;
+
+let vh = windowHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+window.addEventListener('resize', () => {
+  let vh = windowHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+
+//
 // initial fade-in
 setTimeout(() => {
   document.getElementById('watch').classList.add('show');
