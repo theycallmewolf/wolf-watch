a829b87a17fcf9dc152db4eba303ac9953874ff8
bruno-wolf <bruno.m.lobato@gmail.com>
2021-03-03 21:19:26 +0000

    refactor clock controller

--- src/controllers/clock.controller.js
+++ src/controllers/clock.controller.js
@@ -5,47 +5,76 @@ import { getApiData } from '../services/getApiData';
 import { weekDays } from '../utils/memo';
 import { closeModal } from '../controllers/modal.controller';
 
-export const handleClock = async () => {
-  let currentCity;
-  let currentTime = {
-    hours: null,
-    minutes: null,
-    seconds: null,
-    weekday: null,
-    dayOfTheMonth: null,
-  };
+let currentCity;
+let currentTime = {
+  hours: null,
+  minutes: null,
+  seconds: null,
+  weekday: null,
+  dayOfTheMonth: null,
+};
 
-  const setTime = ({ time }) => {
-    const date = new Date(time.replace(' ', 'T'));
-    currentTime.hours = date.getHours();
-    currentTime.minutes = date.getMinutes();
-    currentTime.seconds = new Date().getSeconds();
-    currentTime.weekday = weekDays[date.getDay()];
-    currentTime.dayOfTheMonth = date.getDate();
-  };
-
-  const updateClock = async () => {
-    try {
-      const { location, time, weather } = await getApiData({
-        city: currentCity,
-      });
-
-      document.getElementById('city').innerText = location.title || currentCity;
-      setTime({ time });
-      setClock({
-        hours: currentTime.hours,
-        minutes: currentTime.minutes,
-        seconds: currentTime.seconds,
-        weekDays: currentTime.weekday,
-        dayOfTheMonth: currentTime.dayOfTheMonth,
-      });
-      setWeather({ weather });
-      closeModal();
-    } catch (error) {
-      console.log(error);
+const refreshClock = () => {
+  setInterval(async () => {
+    if (currentTime.seconds < 59) {
+      currentTime.seconds++;
+    } else {
+      currentTime.seconds = 0;
+      if (currentTime.minutes < 59) {
+        currentTime.minutes++;
+      } else {
+        currentTime.minutes = 0;
+        if (currentTime.hours < 23) {
+          currentTime.hours++;
+        } else {
+          currentTime.hours = 0;
+          const { time } = await getApiData({ city: currentCity });
+          setTime({ time });
+        }
+      }
     }
-  };
+    setClock({
+      hours: currentTime.hours,
+      minutes: currentTime.minutes,
+      seconds: currentTime.seconds,
+      weekDays: currentTime.weekday,
+      dayOfTheMonth: currentTime.dayOfTheMonth,
+    });
+  }, 1000);
+};
 
+const setTime = ({ time }) => {
+  const date = new Date(time.replace(' ', 'T'));
+  currentTime.hours = date.getHours();
+  currentTime.minutes = date.getMinutes();
+  currentTime.seconds = new Date().getSeconds();
+  currentTime.weekday = weekDays[date.getDay()];
+  currentTime.dayOfTheMonth = date.getDate();
+};
+
+const updateClock = async () => {
+  try {
+    const { location, time, weather } = await getApiData({
+      city: currentCity,
+    });
+
+    document.getElementById('city').innerText = location.title || currentCity;
+    setTime({ time });
+    setClock({
+      hours: currentTime.hours,
+      minutes: currentTime.minutes,
+      seconds: currentTime.seconds,
+      weekDays: currentTime.weekday,
+      dayOfTheMonth: currentTime.dayOfTheMonth,
+    });
+    setWeather({ weather });
+    closeModal();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const handleClock = async () => {
   currentCity = await getCityNameByIP();
   updateClock();
 
@@ -54,34 +83,5 @@ export const handleClock = async () => {
     updateClock();
   });
 
-  const refreshClock = () => {
-    setInterval(async () => {
-      if (currentTime.seconds < 59) {
-        currentTime.seconds++;
-      } else {
-        currentTime.seconds = 0;
-        if (currentTime.minutes < 59) {
-          currentTime.minutes++;
-        } else {
-          currentTime.minutes = 0;
-          if (currentTime.hours < 23) {
-            currentTime.hours++;
-          } else {
-            currentTime.hours = 0;
-            const { time } = await getApiData({ city: currentCity });
-            setTime({ time });
-          }
-        }
-      }
-      setClock({
-        hours: currentTime.hours,
-        minutes: currentTime.minutes,
-        seconds: currentTime.seconds,
-        weekDays: currentTime.weekday,
-        dayOfTheMonth: currentTime.dayOfTheMonth,
-      });
-    }, 1000);
-  };
-  
   refreshClock();
 };
