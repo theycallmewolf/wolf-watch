e65e7bdfd7fb5744bbfa6507bb70f6ad1354e4af
bruno-wolf <bruno.m.lobato@gmail.com>
2021-03-06 15:13:57 +0000

    adds close button to modal

--- src/controllers/modal.controller.js
+++ src/controllers/modal.controller.js
@@ -5,10 +5,24 @@ const openModal = () => {
 const closeModal = () => {
   document.getElementById('modal').classList.remove('show');
   document.getElementById('location').value = '';
+
+  document.querySelectorAll('#modal > div').forEach(element => {
+    element.classList.add('hide');
+  });
 };
 
 const handleModal = () => {
-  document.getElementById('button-top').addEventListener('click', openModal);
+  document.getElementById('button-top').addEventListener('click', () => {
+    openModal({ content: 'search' });
+  });
+
+  document
+    .getElementById('button-bottom-left')
+    .addEventListener('click', () => {
+      openModal({ content: 'timer' });
+    });
+
+  document.getElementById('close-button').addEventListener('click', closeModal);
 };
 
 export { openModal, closeModal, handleModal };
