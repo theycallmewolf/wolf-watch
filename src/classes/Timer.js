87a6ea43d23b1d7e6fc5eae5c7e33b8752cfd78b
bruno-wolf <bruno.m.lobato@gmail.com>
2021-03-07 16:00:49 +0000

    add start and stop methods to timer

--- src/classes/Timer.js
+++ src/classes/Timer.js
@@ -4,7 +4,7 @@ export default class Timer {
       buttons: {
         increase: document.getElementById('add-minutes'),
         decrease: document.getElementById('remove-minutes'),
-        start: document.getElementById('timer-start'),
+        toggleStart: document.getElementById('timer-start'),
       },
       timer: {
         minutes: document.getElementById('timer-minutes'),
@@ -14,15 +14,25 @@ export default class Timer {
     };
 
     this.timerInSeconds = 25 * 60;
+    this.isActive = false;
+    this.interval = null;
   }
 
   renderTimeOnDisplay() {
-    const minutes = this.timerInSeconds / 60;
-    let seconds = this.timerInSeconds % 60;
-    seconds = String(seconds).padStart(2, '0');
+    const minutes = Math.floor(this.timerInSeconds / 60);
+    const seconds = String(Math.floor(this.timerInSeconds % 60)).padStart(
+      2,
+      '0',
+    );
     this.elements.timer.minutes.innerText = minutes;
     this.elements.timer.seconds.innerText = seconds;
     this.elements.complication.innerText = `${minutes}: ${seconds}`;
+  }
+
+  renderButton() {
+    this.elements.buttons.toggleStart.innerText = this.isActive
+      ? 'stop'
+      : 'start';
   }
 
   increase() {
@@ -41,16 +51,36 @@ export default class Timer {
     this.renderTimeOnDisplay();
   }
 
-  start() {}
+  start() {
+    this.interval = setInterval(() => {
+      if (this.timerInSeconds > 0) {
+        this.elements.buttons.increase.innerText = ' ';
+        this.elements.buttons.decrease.innerText = ' ';
+        this.timerInSeconds--;
+        this.renderTimeOnDisplay();
+        this.isActive = true;
+        this.renderButton();
+      }
+    }, 1000);
+  }
 
-  stop() {}
+  stop() {
+    this.elements.buttons.increase.innerText = '+';
+    this.elements.buttons.decrease.innerText = '-';
+    clearInterval(this.interval);
+    this.isActive = false;
+    this.renderButton();
+  }
 
   reset() {}
 
   execute() {
     this.renderTimeOnDisplay();
+    this.renderButton();
     this.elements.buttons.increase.onclick = () => this.increase();
     this.elements.buttons.decrease.onclick = () => this.decrease();
-    this.elements.buttons.start.onclick = () => this.start();
+    this.elements.buttons.toggleStart.onclick = () => {
+      !this.isActive ? this.start() : this.stop();
+    };
   }
 }
