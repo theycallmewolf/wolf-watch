1e1f8fb4abd6e60ca2703f3a42ffb64d5ea4de2e
bruno-wolf <bruno.m.lobato@gmail.com>
2021-03-09 02:00:45 +0000

    adds app icons

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 import './css/styles.scss';
+import appleTouchIcon from './img/icons/apple-touch-icon.png';
+import favicon16 from './img/icons/favicon-16x16.png';
+import favicon32 from './img/icons/favicon-32x32.png';
+import safariPinnedTab from './img/icons/safari-pinned-tab.svg';
+
 import Clock from './classes/Clock';
 import Modal from './classes/Modal';
 import Rings from './classes/Rings';
@@ -16,6 +21,11 @@ clock.execute();
 modal.execute();
 rings.execute();
 timer.execute();
+
+document.getElementById('appleTouchIcon').href = appleTouchIcon;
+document.getElementById('favicon32').href = favicon32;
+document.getElementById('favicon16').href = favicon16;
+document.getElementById('safariPinnedTab').href = safariPinnedTab;
 
 // greetings
 console.log(`
